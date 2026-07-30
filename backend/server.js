6a21def5a828afec6bf9af8246bd012b269ac1af
alastorhernan <alastorhernan@gmail.com>
2026-07-30 17:59:57 -0300

    Actualizacion automatica

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // 1. Configuración de CORS completa
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
